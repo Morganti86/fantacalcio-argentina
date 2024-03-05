8e00eb54d1fcb77b78cf575b1bf2abc634d75e16
Morganti86 <mmorganti86@gmail.com>
2024-03-05 12:04:20 +0000

    Bangers font

--- src/app/planteles/[name]/page.jsx
+++ src/app/planteles/[name]/page.jsx
@@ -6,6 +6,8 @@ import Image from "next/image";
 export default async function Planteles({ params }) {
   const { name } = params;
   const fantaName = name.replace(/\+|%20/g, " ").toUpperCase();
+  const fantaCrest = fantaName.toLowerCase();
+
   const players = await getFantaPlayers({ fantaName });
 
   const ARQ = players.filter((player) => player.position === "ARQ");
@@ -18,7 +20,7 @@ export default async function Planteles({ params }) {
     <section className={style.container}>
       <Image
         className={style.bigCrest}
-        src={`/Fantateams/${fantaName}.svg`}
+        src={`/Fantateams/${fantaCrest}.svg`}
         width={100}
         height={100}
         alt="imagen"
